dba234eaa40afb25ff5612ae0cf7673ac278515a
John <johnmoreau19@gmail.com>
2024-04-14 15:44:41 -0700

    Fixed the date to include comma

--- src/scripts.js
+++ src/scripts.js
@@ -2,8 +2,11 @@
 let today = new Date
 today.getDate()
 
-let bottomLeftDate = document.querySelector('.bottom-left-date')
-bottomLeftDate.textContent = today.toDateString().slice(3)
+let day = today.getDate() // returns just the day of the month
 
-// Just missing a comma from the date, could do this:
+let bottomLeftDate = document.querySelector('.bottom-left-date')
+// Add a comma and remove the extra day of the week text
+bottomLeftDate.textContent = today.toDateString().slice(3).replace(day, day + ",")
+
+// Just to fix the missing a comma from the date, could do this:
 // https://stackoverflow.com/questions/3552461/how-do-i-format-a-date-in-javascript 
