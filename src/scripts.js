af6e2b43e41aa367038353aa32bab8357583f8e7
John <johnmoreau19@gmail.com>
2024-04-14 15:30:44 -0700

    Fixed sizing of large card

--- src/scripts.js
+++ src/scripts.js
@@ -4,4 +4,7 @@ let today = new Date
 today.getDate()
 
 let bottomLeftDate = document.querySelector('.bottom-left-date')
-bottomLeftDate.textContent = today.toDateString()
+bottomLeftDate.textContent = today.toDateString().slice(3)
+
+// Just missing a comma from the date, could do this:
+// https://stackoverflow.com/questions/3552461/how-do-i-format-a-date-in-javascript 
